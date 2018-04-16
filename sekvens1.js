2c38e9f465e46c71383d97522a5bdccd1a2598ef
rebe4175 <rebe4175@stud.kea.dk>
2018-04-16 12:59:07 +0200

    DETVIRKERFORSATAN

--- sekvens1.js
+++ sekvens1.js
@@ -1,43 +1,92 @@
-$(window).on("load", begynd);
+$(window).on("load", sidenVises)
 
-function begynd() {
-    $("#start").on("click", movingFox);
-    $("#start").on("click", movingBear);
+function sidenVises() {
 
-    $("#stop").on("click", stopFox);
-    $("#stop").on("click", stopBear);
-}
+    console.log("sidenVises")
 
-function movingFox() {
 
-    console.log("moving_fox");
     $("#fox_container").addClass("moving_fox");
     $("#fox_sprite").addClass("walk_fox");
-
-}
-
-function movingBear() {
-
-    console.log("moving_bear");
     $("#bear_container").addClass("moving_bear");
     $("#bear_sprite").addClass("walk_bear");
 
+    $("#fox_container").on("animationend", stopFox);
+    $("#bear_container").on("animationend", stopBear);
 }
+
 
 function stopFox() {
 
-    console.log("fox_center");
+
+    console.log("stopFox");
+
+    $("#fox_container").off("animationend", stopFox);
+
     $("#fox_container").removeClass("moving_fox");
     $("#fox_sprite").removeClass("walk_fox");
+
     $("#fox_container").addClass("fox_center");
 
+    $("#fox_container").on("animationend", turnFox);
 }
 
 function stopBear() {
 
-    console.log("bear_center");
+
+    console.log("stopBear");
+
+    $("#bear_container").off("animationend", stopBear);
+
     $("#bear_container").removeClass("moving_bear");
     $("#bear_sprite").removeClass("walk_bear");
     $("#bear_container").addClass("bear_center");
 
+    $("#fox_container").on("animationend", turnFox);
+
+}
+
+function turnFox() {
+
+
+    console.log("turnFox");
+
+    $("#fox_container").off("animationend", turnFox);
+
+    $("#fox_container").removeClass("fox_center");
+    $("#fox_sprite").addClass("fox_turn");
+
+    $("#fox_container").on("animationend", foxRun);
+}
+
+function foxRun() {
+
+    console.log("foxRun");
+
+    $("#fox_container").off("animationend", foxRun);
+
+
+    $("#fox_container").removeClass("fox_center");
+    $("#fox_container").addClass("fox_run_move");
+    $("#fox_sprite").addClass("fox_run")
+
+    $("#bear_container").removeClass("bear_center");
+    $("#bear_container").addClass("bear_attack");
+
+    $("#fox_container").on("animationend", foxLose);
+}
+
+
+function foxLose() {
+
+    console.log("foxLose");
+
+    $("#fox_container").off("animationend", foxLose);
+
+
+    $("#fox_container").removeClass("fox_run_move");
+    $("#fox_sprite").removeClass("fox_run")
+
+    $("#bear_container").removeClass("bear_attack");
+
+    $("#fox_sprite").addClass("fox_lose");
 }
