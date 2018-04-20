09b38ed3d6389969c640f4ad08227c7f85459afe
rebe4175 <rebe4175@stud.kea.dk>
2018-04-20 13:24:17 +0200

    megetVirker

--- sekvens1.js
+++ sekvens1.js
@@ -1,3 +1,8 @@
+var taeller = 0;
+
+var erDerKlikketNok = false;
+
+
 $(window).on("load", sidenVises);
 
 function sidenVises() {
@@ -6,6 +11,14 @@ function sidenVises() {
 
     $("#quietmusic")[0].play();
 
+    $("#fox_sprite2").hide();
+
+    $("#valg1").hide();
+    $("#valg2").hide();
+    $("#charge").hide();
+
+
+    $("#clawmarks").hide();
     $("#fox_container").addClass("moving_fox");
     $("#fox_sprite").addClass("walk_fox");
     $("#bear_container").addClass("moving_bear");
@@ -28,7 +41,7 @@ function stopFox() {
 
     $("#fox_container").addClass("fox_center");
 
-    $("#fox_container").on("animationend", turnFox);
+
 }
 
 function stopBear() {
@@ -44,7 +57,20 @@ function stopBear() {
 
     $("#growl")[0].play();
 
-    $("#fox_container").on("animationend", turnFox);
+    ValgEt();
+
+
+}
+
+function ValgEt() {
+
+
+    $("#valg1").fadeIn();
+    $("#valg2").fadeIn();
+
+
+    $("#valg1").on("click", turnFox);
+    $("#valg2").on("click", foxCharging);
 
 }
 
@@ -55,10 +81,15 @@ function turnFox() {
 
     console.log("turnFox");
 
+    $("#valg1").fadeOut();
+    $("#valg2").hide();
+
     $("#fox_container").off("animationend", turnFox);
 
     $("#fox_container").removeClass("fox_center");
     $("#fox_sprite").addClass("fox_turn");
+
+
 
     $("#fox_container").on("animationend", foxRun);
 }
@@ -76,6 +107,9 @@ function foxRun() {
 
     $("#bear_container").removeClass("bear_center");
     $("#bear_container").addClass("bear_attack");
+    $("#clawmarks").show();
+    $("#clawmarks").addClass("klo");
+
 
     $("#fox_container").on("animationend", foxLose);
 }
@@ -85,35 +119,24 @@ function foxLose() {
 
     console.log("foxLose");
 
+
+
     $("#fox_container").off("animationend", foxLose);
+
+    $("#clawmarks").hide();
 
 
     $("#fox_container").removeClass("fox_run_move");
     $("#fox_sprite").removeClass("fox_run")
-
     $("#bear_container").removeClass("bear_attack");
 
     $("#fox_sprite").addClass("fox_lose");
 
     $("#foxdies")[0].play();
 
-    $("#fox_container").on("animationend", foxyfox);
-
-
 
 }
 
-
-function foxyfox() {
-
-    console.log("foxyfox");
-
-    $("#fox_container").off("animationend", foxyfox);
-
-    $("#fox_container").on("animationend", foxLose);
-
-
-}
 
 
 //valgmulighed 2
@@ -124,12 +147,113 @@ function foxAttack() {
 
     $("#fox_container").off("animationend", foxAttack);
 
-    $("#fox_container").addClass("fox_attack");
+    $("#fox_container").removeClass("fox_center");
 
-    $("#fox_container").on("animationend", bearLose);
+    $("#fox_container").addClass("fox_attack");
+    $("#clawmarks").show();
+    $("#clawmarks").addClass("klo_fox");
+
 
 
 }
+
+function kanKlikke() {
+
+    console.log("kanKlikke");
+
+    $("#charge").on("click", klikPaaKnap);
+}
+
+function klikPaaKnap() {
+
+    console.log("klik paa knap");
+
+
+    $("#charge").off("click", klikPaaKnap);
+
+    $("#charge").addClass("puls");
+
+    $("#charge").on("animationend", foxCharge);
+}
+
+function foxCharging() {
+
+    console.log("foxCharging");
+
+    $("#fox_container").off("animationend", foxCharging);
+
+    $("#valg1").hide();
+    $("#valg2").fadeOut();
+
+    $("#charge").delay(1000).fadeIn();
+
+    setTimeout(tidenergaaet, 10000);
+    kanKlikke();
+}
+
+function foxCharge() {
+    console.log("foxCharge");
+
+    //valgmulighed 2
+    $("#charge").off("animationend", foxCharge);
+    $("#charge").removeClass("puls");
+    $("#fox_container").addClass("fox_charge");
+    taeller++;
+    console.log("har klikket " + taeller + " gange");
+    if (taeller >= 10) {
+        console.log("fox wins");
+        erDerKlikketNok = true;
+
+        foxfaerdig();
+
+    } else {
+
+        // console.log("klik igen");
+
+        erDerKlikketNok = false;
+
+        kanKlikke();
+    }
+
+}
+
+function tidenergaaet() {
+
+    console.log("tiden er gaaet");
+
+    if (erDerKlikketNok == false) {
+
+        foxfaerdig();
+
+    }
+
+
+}
+
+function foxfaerdig() {
+
+    console.log("faerdig");
+
+    if (erDerKlikketNok == true) {
+
+        console.log("fox winning");
+        foxWins();
+
+    } else {
+
+        (erDerKlikketNok == false);
+        console.log("fox losing");
+        foxLose();
+
+    }
+}
+
+
+function foxAttack() {
+    $("fox_container").removeClass("fox_charge");
+    $("fox_container").addClass("fox_charge");
+}
+
 
 
 function bearLose() {
@@ -141,6 +265,60 @@ function bearLose() {
     $("#fox_container").removeClass("fox_attack");
     $("#bear_sprite").addClass("bear_dies");
 
+
+}
+
+
+function foxWins() {
+
+    console.log("foxWins");
+
+    $("#fox_container").off("animationend", foxWins);
+
+    $("#fox_container").addClass("fox_attack");
+
+    $("#fox_container").on("animationend", foxVictory);
+
+
+}
+
+function foxVictory() {
+
+    console.log("fox victory");
+
+    $("#fox_sprite").hide();
+    $("#fox_sprite2").show();
+
+    $("#fox_container").off("animationend", foxWins);
+
+    $("#fox_container").removeClass("fox_attack");
+    $("#fox_sprite2").addClass("win_fox");
+
+
+}
+
+function foxLoseAgain() {
+
+    console.log("bearLose");
+
+    $("#fox_container").off("animationend", bearLose);
+
+    $("#fox_container").removeClass("fox_attack");
+    $("#bear_sprite").addClass("bear_dies");
+
+
+}
+
+
+
+function bearWin() {
+
+    console.log("bearWin");
+
+    $("#fox_container").off("animationend", bearLose);
+
+    $("#fox_container").removeClass("fox_attack");
+    $("#bear_sprite").addClass("bear_dies");
 
 
 }
